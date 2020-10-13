88e02a8ff700746b02647ccf8fa8d09482a611bf
Kirkland <kirklandday226@gmail.com>
2020-10-12 19:36:05 -0700

    I have not done anything

---
+++ Unity_ONW/Assets/Begin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Begin : MonoBehaviour
+{
+
+    public GameObject myObj;
+    public Transform myObjPos;
+
+    static Begin() {
+        EditorApplication.update += Update;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    static void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Debug.Log("Test");
+        }
+
+        if (Input.GetKeyDown("a")) {
+            Debug.Log("Testh");
+        }
+    }
+}
